f03103140d30446d98a956bf4834e23afd5222e4
igor <igopko1996@gmail.com>
2018-04-20 01:29:56 +0300

    Add files via upload

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,4 @@
-const moongoose=require("mongoose");
+const mongoose = require("mongoose");
 const express =require("express");
 
 const router = express.Router();
@@ -12,3 +12,35 @@ router.get("/", (req, res)=>{
 
 
 module.exports= router;
+const User = require("../models/user");
+router.post("/", (req, res)=>{
+    const user = new User({
+        _id: mongoose.Types.ObjectId(),
+        name: req.body.name
+    });
+
+    user.save().then(result =>{
+        console.log(result);
+        res.status(201).json({
+            "message":"Handling POST request to users", 
+            "createdUser":result
+        });
+    }).catch(err=> {
+        console.log(err)
+        res.status(500).json({
+            error:err
+        });
+    });
+
+   
+})
+router.get('/:userId', (req, res)=>{
+    const _id =req.params.userId;
+    User.findById(_id).then(userData=>{
+        console.log(userData);
+        res.status(200).json(userData)
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json({error:err})
+    });
+})
